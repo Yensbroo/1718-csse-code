2550f3f1f5bf0f3e58c827931ba149dd1232c69e
Yensbroo <yens.broothaers@student.arteveldehs.be>
2017-10-17 11:57:40 +0200

    opdrachtweek3

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace opdrachtweek3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber, i;
+            Console.WriteLine("enter a value:");
+            firstNumber = Convert.ToInt32(Console.ReadLine());
+            List<int> list = new List<int>();
+            for(i = 0; i <= 10; i++)
+            {
+                int result = firstNumber * i;
+               Console.WriteLine("{0} * {1} = {2}", i, firstNumber, result);
+               list.Add(result);
+            }
+        }
+    }
+}
